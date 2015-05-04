33d9e14b4ccdaba3e6d18e76e4d0a703438f67f8
David Kreth Allen <davidkrethallen@gmail.com>
2015-05-04 07:54:02 -0500

    created new unit test project with simple test

---
+++ GitUnitTest1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitUnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
